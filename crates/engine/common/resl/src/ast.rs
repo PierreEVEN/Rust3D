dccd321ac5991d798dd39d79755a44e7b0f245ae
PierreEVEN <evenpierre@orange.fr>
2023-09-27 23:12:50 +0200

    wip shb lexer

--- crates/engine/common/resl/src/ast.rs
+++ crates/engine/common/resl/src/ast.rs
@@ -1,3 +1,47 @@
+use std::collections::HashSet;
+
+#[derive(Debug)]
+pub struct ListOf<T> {
+    items: Vec<T>,
+}
+
+impl<T> ListOf<T> {
+    pub fn new() -> Self { Self { items: vec![] } }
+
+    pub fn push(mut self, instr: T) -> Self {
+        self.items.push(instr);
+        self
+    }
+
+    pub fn concat(mut self, other: &mut Self) -> Self {
+        self.items.append(&mut other.items);
+        self
+    }
+}
+
+#[derive(Debug)]
+pub struct RenderPassGroup {
+    render_passes: HashSet<String>,
+}
+
+impl RenderPassGroup {
+    pub fn new() -> Self { Self { render_passes: Default::default() } }
+    pub fn add(mut self, pass: String) -> Self {
+        self.render_passes.insert(pass);
+        self
+    }
+}
+
+#[derive(Debug)]
+pub enum Instruction {
+    Pragma(String, Value),
+    Global(RenderPassGroup, ListOf<HlslInstruction>),
+    Vertex(RenderPassGroup, ListOf<HlslInstruction>),
+    Fragment(RenderPassGroup, ListOf<HlslInstruction>),
+    Compute(RenderPassGroup, ListOf<HlslInstruction>),
+}
+
+#[derive(Debug)]
 pub enum Value {
     None,
     Integer(i64),
@@ -5,25 +49,30 @@ pub enum Value {
     String(String),
 }
 
-pub enum Type {
-    Bool,
-    Int,
-    Uint,
-    Dword,
-    Half,
-    Float,
-    Double,
-    Min16Float,
-    Min10Float,
-    Min16Int,
-    Min12Int,
-    Min16Uint,
-    Uint64t,
-    Int64t,
-    Float16t,
-    Uint16t,
-    Int16t,
-    String,
-    Matrix,
-    Vector
+#[derive(Debug)]
+pub enum HlslInstruction {
+    Struct(String, ListOf<StructureField>),
+    Function(String, Function),
+    Pragma(String, Value)
+}
+
+#[derive(Debug)]
+pub struct StructureField {
+    pub struct_type: String,
+    pub name: String,
+    pub value: Option<String>,
+    pub attribute: Option<String>
+}
+
+#[derive(Debug)]
+pub struct Function {
+    pub return_type: String,
+    pub params: Vec<FunctionParameter>,
+    pub content: String,
+}
+
+#[derive(Debug)]
+pub struct FunctionParameter {
+    pub param_type: String,
+    pub name: String,
 }
--- crates/engine/common/resl/src/lib.rs
+++ crates/engine/common/resl/src/lib.rs
@@ -1,8 +1,126 @@
+mod ast;
+
 use lalrpop_util::lalrpop_mod;
 
 lalrpop_mod!(pub language); // synthesized by LALRPOP
 
+fn get_error_location(code: &str, token: usize) -> (usize, usize) {
+    let mut line = 0;
+    let mut token_to_last_line = 0;
+    let mut elapsed_tokens = 0;
+    for chr in code.chars() {
+        if chr == '\n' {
+            line += 1;
+            token_to_last_line = elapsed_tokens;
+        }
+        elapsed_tokens += 1;
+        if elapsed_tokens >= token {
+            break;
+        }
+    }
+    (line, token - token_to_last_line)
+}
+
 #[test]
 fn calculator1() {
-    assert!(language::QuoteStringParser::new().parse(r#""hello vous allez bien ?""#).is_ok());
+
+    let code = r#"
+    #pragma once twice
+    #pragma test two
+    #pragma ENABLE_HLSL
+    #pragma integer 4
+    #pragma version "4.5.8.9 test"
+
+    global(test, "shader a", shader_b) {
+        struct VSInput
+        {
+            float2 aPos 	: POSITION;
+            float2 aUV 		: TEXCOORD;
+            float4 aColor 	: COLOR;
+        }
+
+        struct PushConsts
+        {
+            float2 uScale;
+            float2 uTranslate;
+        }
+
+        VsToFs main(VSInput input)
+        {
+            VsToFs Out;
+            Out.Color	= input.aColor;
+            Out.UV 		= input.aUV;
+            Out.Pos 	= float4(input.aPos * pc.uScale + pc.uTranslate, 0, 1);
+            return Out;
+        }
+    }
+
+    vertex(example) {
+        // declaration of functions
+
+        // data structure : before vertex shader (mesh info)
+        struct vertexInfo
+        {
+            float3 position : POSITION;
+            float2 uv: TEXCOORD0;
+            float3 color : COLOR;
+        }
+
+        // data structure : vertex shader to pixel shader
+        // also called interpolants because values interpolates through the triangle
+        // from one vertex to another
+        struct v2p
+        {
+            float4 position : SV_POSITION;
+            float3 uv : TEXCOORD0;
+            float3 color : TEXCOORD1;
+        }
+
+        // uniforms : external parameters
+        sampler2D MyTexture; //@TODO : property are not supported
+        float2 UVTile;
+        matrix4x4 worldViewProjection;
+
+        // vertex shader function
+        v2p vertexShader(vertexInfo input)
+        {
+            v2p output;
+            output.position = mul(worldViewProjection, float4(input.position,1.0));
+            output.uv = input.uv * UVTile;
+            output.color = input.color;
+            return output;
+        }
+
+        // pixel shader function
+        float4 pixelShader(v2p input) : SV_TARGET
+        {
+            float4 color = tex2D(MyTexture, input.uv);
+            return color * input.color;
+        }
+    }
+
+    "#;
+
+    let parsed = match language::InstructionListParser::new().parse(code) {
+        Ok(res) => res,
+        Err(e) => match e {
+            lalrpop_util::ParseError::UnrecognizedToken {token, expected} => {
+                let (a, b, c) = token;
+                // Comment lire le contenu de UnrecognizedToken ?
+                let (line, column) = get_error_location(code, a);
+
+                panic!("Unrecognized token at {}:{} : Found {:?}\nexpected {:?}", line, column, b, expected)
+            },
+            lalrpop_util::ParseError::InvalidToken {location} => {
+                let (line, column) = get_error_location(code, location);
+                panic!("Unrecognized token at {}:{} : '{:?}'", line, column, code.chars().nth(location).unwrap())
+
+            },
+            _ => {
+                panic!("Compilation failed:\n{:?}", e)
+            }
+        }
+    };
+
+    println!("${:?}", parsed);
 }
